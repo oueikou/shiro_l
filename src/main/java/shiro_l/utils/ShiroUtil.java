d569893158bf8239766890b2395372c4f33689f0
yongheng.wang <yongheng@yongheng-PC>
2016-12-20 15:40:20 +0800

    ShiroUtil 登录方法异常抓取

--- src/main/java/shiro_l/utils/ShiroUtil.java
+++ src/main/java/shiro_l/utils/ShiroUtil.java
@@ -5,6 +5,10 @@ import org.slf4j.LoggerFactory;
 
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.ExcessiveAttemptsException;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.LockedAccountException;
+import org.apache.shiro.authc.UnknownAccountException;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.config.IniSecurityManagerFactory;
 import org.apache.shiro.mgt.SecurityManager;
@@ -36,8 +40,24 @@ public class ShiroUtil {
 		try {
 			// 6.登录
 			subject.login(token);
-		} catch (AuthenticationException e) {
-			logger.error("shiro登录异常!", e);
+		} catch (UnknownAccountException uae) {
+			logger.error("--账号不正确--");
+			uae.printStackTrace();
+		} catch (IncorrectCredentialsException ice) {
+			logger.error("--密码不正确--");
+			ice.printStackTrace();
+		} catch (LockedAccountException lae) {
+			logger.error("--账号已被锁--");
+			lae.printStackTrace();
+		} catch (ExcessiveAttemptsException eae) {
+			logger.error("--过多尝试异常--");
+			eae.printStackTrace();
+		} catch (AuthenticationException ae) {
+			logger.error("--认证异常--");
+			ae.printStackTrace();
+		} catch (Exception e) {
+			logger.error("--登录发生未知异常--");
+			e.printStackTrace();
 		}
 		return subject;
 	}
